ba1bc02f32760e1c5516d07414ebe645006a48f9
matheusraz <matheusraz@users.noreply.github.com>
2023-11-06 22:18:58 -0300

    fix: :hammer: Fix amount of shortened chars
    Signed-off-by: matheusraz <matheusraz@users.noreply.github.com>

--- script.sh
+++ script.sh
@@ -6,6 +6,6 @@ SHA=$(curl -L \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer ${GITHUB_TOKEN}" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/$GITHUB_REPOSITORY/commits/$REF_NAME --silent| jq '.sha' | sed s/\"//g | cut -c 1-8)
+  https://api.github.com/repos/$GITHUB_REPOSITORY/commits/$REF_NAME --silent| jq '.sha' | sed s/\"//g | cut -c 1-7)
 
 echo "::set-output name=sha::$SHA"
