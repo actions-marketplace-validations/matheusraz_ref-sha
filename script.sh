333acc4f019b776dc009215cc416d6bc69415c74
matheusraz <matheusraz@users.noreply.github.com>
2023-11-06 22:17:13 -0300

    init: :tada: First commit with implementation
    Signed-off-by: matheusraz <matheusraz@users.noreply.github.com>

---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+REF_NAME=$INPUT_REF
+
+SHA=$(curl -L \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/$GITHUB_REPOSITORY/commits/$REF_NAME --silent| jq '.sha' | sed s/\"//g | cut -c 1-8)
+
+echo "::set-output name=sha::$SHA"
