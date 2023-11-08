dc5adce7f2dabfda596b6d1d2cb9975023b23006
matheusraz <matheusraz@users.noreply.github.com>
2023-11-08 11:17:32 -0300

    chore: :zap: Set commit length based on users choice
    Signed-off-by: matheusraz <matheusraz@users.noreply.github.com>

--- script.sh
+++ script.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+REF_LENGTH=$INPUT_LENGTH
 REF_NAME=$INPUT_REF
 
 SHA=$(curl -L \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+  -H "Authorization: Bearer $GITHUB_TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/$GITHUB_REPOSITORY/commits/$REF_NAME --silent| jq '.sha' | sed s/\"//g | cut -c 1-7)
+  https://api.github.com/repos/$GITHUB_REPOSITORY/commits/$REF_NAME --silent| jq '.sha' | sed s/\"//g | cut -c 1-$REF_LENGTH)
 
 echo "::set-output name=sha::$SHA"
+
+echo "SHA=$SHA" >> GITHUB_ENV
