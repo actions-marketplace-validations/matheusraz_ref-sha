7aeb13fb1561ff5c9a495002cff73db83d2592f4
matheusraz <matheusraz@users.noreply.github.com>
2023-11-08 11:59:55 -0300

    fix: :hammer: Passing inputs as args
    Signed-off-by: matheusraz <matheusraz@users.noreply.github.com>

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-REF_NAME=$INPUT_REF
-REF_LENGTH=$INPUT_LENGTH
+REF_NAME=$1
+REF_LENGTH=$2
 
 SHA=$(curl -L \
   -H "Accept: application/vnd.github+json" \
@@ -9,4 +9,4 @@ SHA=$(curl -L \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/$GITHUB_REPOSITORY/commits/$REF_NAME --silent| jq '.sha' | sed s/\"//g | cut -c 1-$REF_LENGTH)
 
-echo "sha=$SHA" >> GITHUB_OUTPUT
+# echo "sha=$SHA" >> GITHUB_OUTPUT
