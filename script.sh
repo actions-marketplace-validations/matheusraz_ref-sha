9c49cf724a3014138343cb23b0484f0c866336ab
matheusraz <matheusraz@users.noreply.github.com>
2023-11-08 11:56:06 -0300

    fix: :hammer: Fix bad subst error
    Signed-off-by: matheusraz <matheusraz@users.noreply.github.com>

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ REF_LENGTH=$INPUT_LENGTH
 
 SHA=$(curl -L \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer $GITHUB_TOKEN" \
+  -H "Authorization: Bearer ${GITHUB_TOKEN}" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/$GITHUB_REPOSITORY/commits/$REF_NAME --silent| jq '.sha' | sed s/\"//g | cut -c 1-$REF_LENGTH)
 
